419ed7f8802d108821f508732d09ae564c60facf
unknown <weibo.zhang@teng.com>
2019-03-15 10:10:20 +0800

    add

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="index">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
